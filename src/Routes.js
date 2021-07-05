095e03a2b5dd151bd8bb71aabc5d290a60dd1a8f
gwanyong <78327961+gwanyong@users.noreply.github.com>
2021-07-05 17:43:40 +0900

    formating 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Detail from "./pages/Detail/Detail";
-import Login from "./pages/Login/Login";
-import Main from "./pages/Main/Main";
-import ShoppingList from "./pages/Shopping/shoppingList";
-import SignUp from "./pages/SignUp/SignUp";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './pages/Detail/Detail';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import ShoppingList from './pages/Shopping/shoppingList';
+import SignUp from './pages/SignUp/SignUp';
 
 class Routes extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/Login" component={Login} />
-                    <Route exact path="/SignUp" component={SignUp} />
-                    <Route exact path="/" component={Main} />
-                    <Route exact path="/List" component={ShoppingList} />
-                    <Route exact path="/Detail" component={Detail} />
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/SignUp" component={SignUp} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/List" component={ShoppingList} />
+          <Route exact path="/Detail" component={Detail} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Routes;
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 class SignUp extends React.Component {}
 
