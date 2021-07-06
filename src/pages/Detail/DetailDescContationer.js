7a0798506e3d68973701afba0f35000bc0185a4c
unseo Jang <unseojang@seoun-iui-maegbug.local>
2021-07-06 21:34:43 +0900

    Add : 3차 detail layout 완료 commit

--- src/pages/Detail/DetailDescContationer.js
+++ src/pages/Detail/DetailDescContationer.js
@@ -1,19 +1,77 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import DetailDateDisplay from './DetailDateDisplay';
 
 export class DetailDescContationer extends Component {
   render() {
     return (
-      <section className="detail-desc-wrap">
-        <article>
-          <img src="https://firebasestorage.googleapis.com/v0/b/jyg-custom-seoul-app/o/frontend%2Fdescriptions%2Fweb%2Fporkbelly-fresh1.png?alt=media"></img>
-        </article>
-        <DetailDateDisplay></DetailDateDisplay>
-        <article>
-          <img src="https://firebasestorage.googleapis.com/v0/b/jyg-custom-seoul-app/o/frontend%2Fdescriptions%2Fweb%2Fporkbelly-fresh2.png?alt=media" />
-        </article>
-      </section>
+      <div className="detail-product-wrap">
+        <section className="detail-desc-wrap">
+          <article>
+            <img src="https://firebasestorage.googleapis.com/v0/b/jyg-custom-seoul-app/o/frontend%2Fdescriptions%2Fweb%2Fporkbelly-fresh1.png?alt=media"></img>
+          </article>
+          <DetailDateDisplay></DetailDateDisplay>
+          <article>
+            <img src="https://firebasestorage.googleapis.com/v0/b/jyg-custom-seoul-app/o/frontend%2Fdescriptions%2Fweb%2Fporkbelly-fresh2.png?alt=media" />
+          </article>
+        </section>
+        <section className="detail-review">
+          <ul>
+            <li className="detail-review-photo-container">
+              <Link to="#!">
+                <div className="detail-review-box">
+                  <div className="detail-review-info">
+                    <h6>맛있어요~~</h6>
+                    <div className="detail-review-content-wrap">
+                      <p>잘 먹었습니당...</p>
+                    </div>
+                    <div className="detail-review-customer">
+                      <p>심익현</p>
+                      <p className="count">1회 구매</p>
+                      <time dateTime="2021-07-06">2021-07-06</time>
+                    </div>
+                  </div>
+                  <div className="detail-review-photo-wrap">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/jyg-custom.appspot.com/o/reviews%2F20210706_004726_dM3DZtguSt6BzAff9yc3s6_3abba3fe-f54e-4f3c-920d-01b1347e5c84.jpg?alt=media&token=e5753d84-9958-4837-9481-9337d288b3aa" />
+                  </div>
+                </div>
+                <div className="detail-review-width-product">
+                  <div className="product-wrap-num">
+                    함께 구매하신 상품<span className="num">(3)</span>
+                  </div>
+                  <div className="product-name-num">초신선 돼지고기 x 3</div>
+                </div>
+              </Link>
+            </li>
+            <li className="detail-review-container">
+              <Link to="#!">
+                <div className="detail-review-box">
+                  <div className="detail-review-info">
+                    <h6>맛있어요~~</h6>
+                    <div className="detail-review-content-wrap">
+                      <p>잘 먹었습니당...</p>
+                    </div>
+                    <div className="detail-review-customer">
+                      <p>심익현</p>
+                      <p className="count">1회 구매</p>
+                      <time dateTime="2021-07-06">2021-07-06</time>
+                    </div>
+                  </div>
+                </div>
+                <div className="detail-review-width-product">
+                  <div className="product-wrap-num">
+                    함께 구매하신 상품<span className="num">(3)</span>
+                  </div>
+                  <div className="product-name-num">초신선 돼지고기 x 3</div>
+                </div>
+              </Link>
+            </li>
+          </ul>
+          <button className="detail-review-more"></button>
+        </section>
+        <section className="detail-info"></section>
+      </div>
     );
   }
 }
