5aaedd663a98a0d118cd1726893b16ac5d78ebaa
gwanyong <78327961+gwanyong@users.noreply.github.com>
2021-07-06 15:23:04 +0900

    cart layout 일부 구현

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Cart from './pages/Cart/Cart';
 import Detail from './pages/Detail/Detail';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
@@ -16,6 +17,7 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/List" component={ShoppingList} />
           <Route exact path="/Detail" component={Detail} />
+          <Route exact path="/Cart" component={Cart} />
         </Switch>
       </Router>
     );
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,8 +1,74 @@
 import React, { Component } from 'react';
+import './Cart.scss';
 
 export class Cart extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="cart">
+        <p className="cartTitle">장바구니</p>
+        <div className="sectionWrap">
+          <section className="cartContainer">
+            <div className="listHeader">
+              <div className="header-info">상품정보</div>
+              <div className="header-count">수량</div>
+              <div className="header-price">가격</div>
+            </div>
+            <ul>
+              <li className="itemList">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/jyg-custom-seoul-app/o/frontend%2Fthumbnails%2Ftransparent_background%2Fporkbelly-clean-list.png?alt=media"
+                  alt="itemIm"
+                />
+                <div>
+                  <p>초신선 무항생제 돼지 삼겹살 구이용</p>
+                  <p>보통(16mm)</p>
+                </div>
+                <p>600g 기준</p>
+                <div className="itemCounter">
+                  <div>
+                    <i class="fas fa-minus"></i>
+                  </div>
+                  <p>1</p>
+                  <div>
+                    <i class="fas fa-plus"></i>
+                  </div>
+                </div>
+                <p>19,800원</p>
+                <i class="far fa-window-close"></i>
+              </li>
+            </ul>
+          </section>
+          <section className="paymentContainer">
+            <div className="paymentBox">
+              <div className="productPrice">
+                <p className="productPrice__text">총 상품 금액</p>
+                <p className="productPrice__price">36,600원</p>
+              </div>
+              <div className="shippingPrice">
+                <div className="shippingPrice-total">
+                  <p>총 배송비</p>
+                  <p>0원</p>
+                </div>
+                <div className="shippingPrice-basic">
+                  <p>기본 배송비</p>
+                  <p>2500원</p>
+                </div>
+                <div className="firstPayment">
+                  <p>첫구매 무료배송</p>
+                  <p>-100%</p>
+                </div>
+              </div>
+              <p classNam="paymentMessage">
+                첫구매 무료배송 혜택이 적용되었습니다.
+              </p>
+              <div className="totalPrice"></div>
+              <button className="paymentBtn">전체상품 주문하기</button>
+              <button className="keepShoppingBtn">쇼핑계속하기</button>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
   }
 }
 
