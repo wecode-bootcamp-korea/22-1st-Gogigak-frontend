ac6e0fe54b326cd80b11d2fe26efbe9160cf2266
lee5992 <dlwogus432@gmail.com>
2021-07-06 15:03:28 +0900

    Add : 상단네비바 최초 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import './Nav.scss';
 
 export class Nav extends Component {
   render() {
-    return <div></div>;
+    return (
+      <frameElement>
+        <div>상단배너</div>
+        <header>
+          <div className="navigationContainer">
+            <div className="navigationLeft">
+              <img alt="네비아이콘" />
+              <ul className="navigationMainMenu">
+                <li>쇼핑하기</li>
+                <li>이용가이드</li>
+                <li>이벤트</li>
+              </ul>
+            </div>
+            <div className="navigationRight">
+              <ul>
+                <li>공지사항</li>
+                <li>고객센터</li>
+              </ul>
+              <div className="navigationSubMenuSplit"></div>
+              <ul>
+                <li>공지사항</li>
+                <li>고객센터</li>
+              </ul>
+            </div>
+          </div>
+        </header>
+      </frameElement>
+    );
   }
 }
 
