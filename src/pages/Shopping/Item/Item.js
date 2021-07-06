12fa2e87f6d7976186873f86d16e60d0c964d159
gwanyong <78327961+gwanyong@users.noreply.github.com>
2021-07-06 15:29:48 +0900

    listpage 레이아웃 리팩토링

--- src/pages/Shopping/Item/Item.js
+++ src/pages/Shopping/Item/Item.js
@@ -4,21 +4,19 @@ import './Item.scss';
 class Item extends React.Component {
   render() {
     return (
-      <>
-        <li className="item">
-          <div className="imgContainer">
-            <img
-              src="https://firebasestorage.googleapis.com/v0/b/jyg-custom-seoul-app/o/frontend%2Fthumbnails%2Ftransparent_background%2Fmanilaclam-water-detail.png?alt=media"
-              alt="itemImg"
-            />
-            <div className="cartBackground">
-              <i class="fas fa-shopping-cart"></i>
-            </div>
+      <li className="item">
+        <div className="imgContainer">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/jyg-custom-seoul-app/o/frontend%2Fthumbnails%2Ftransparent_background%2Fmanilaclam-water-detail.png?alt=media"
+            alt="itemImg"
+          />
+          <div className="cartBackground">
+            <i class="fas fa-shopping-cart"></i>
           </div>
-          <h6 className="itemTitlte">초신선 왕바지락</h6>
-          <p className="priceInfo">기준가 6,500원/500g</p>
-        </li>
-      </>
+        </div>
+        <h6 className="itemTitlte">초신선 왕바지락</h6>
+        <p className="priceInfo">기준가 6,500원/500g</p>
+      </li>
     );
   }
 }
