804c2286438d91de5c90d631d9a89938c3eda199
lee5992 <dlwogus432@gmail.com>
2021-07-07 14:13:15 +0900

    Add : 메인레이아웃 작성 중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Item from '../Shopping/Item/Item';
 import './Main.scss';
 
 export default class Main extends Component {
@@ -16,7 +17,18 @@ export default class Main extends Component {
             <div>신선플랜</div>
           </div>
         </section>
-        <section></section>
+        <section className="bestItemContainer">
+          <h5>정육각 베스트 상품</h5>
+          <ul className="bestItems">
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+          </ul>
+        </section>
+        <div className="shoppingBanner"></div>
       </div>
     );
   }
