885adee1ea430f30981fa22e0f2239b94082cbc6
lee5992 <dlwogus432@gmail.com>
2021-07-07 20:17:44 +0900

    Add : 메인페이지 초기버전 레이아웃 및 슬라이더 초기버전 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,6 +3,51 @@ import Item from '../Shopping/Item/Item';
 import './Main.scss';
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.slideContainer = React.createRef();
+  }
+
+  slideByNumber(e) {
+    this.slideContainer.current.style.transition =
+      'width 2s, height 2s, background-color 2s, transform 1s';
+
+    this.slideContainer.current.style.transform = `translateX(-${
+      576 * e.target.innerHTML
+    }px)`;
+
+    // console.log(this.slideContainer.current.style);
+  }
+  // slideNext() {
+  // console.log(carousel);
+  // let index = 0;
+  // if (index === 0) {
+  //   this.slideContainer.current.style.transform = `translateX(-${576}px)`;
+  //   index = index + 1;
+  // } else {
+  //   this.slideContainer.current.style.transform = `translateX(-${576}px)`;
+  //   index = index + 1;
+  // }
+  // console.log(this.slideContainer.current.style.transform);
+  // console.log(index);
+  // }
+
+  // slidePrevious() {
+  //   // console.log(carousel);
+
+  //   let index = 0;
+
+  //   if (index === 0) {
+  //     this.slideContainer.current.style.transform = `translateX(-${576 * 2}px)`;
+  //     index = index + 1;
+  //   } else {
+  //     this.slideContainer.current.style.transform = `translateX(+${576}px)`;
+  //     index = index + 1;
+  //   }
+  //   console.log(this.slideContainer.current.style.transform);
+  //   console.log(index);
+  // }
+
   render() {
     return (
       <div className="mainPage">
@@ -11,11 +56,26 @@ export default class Main extends Component {
         </section>
         <section className="eventBannerContainer">
           <div className="mainEvent">
-            <div>당일배송</div>
+            <div className="slideOverFlow">
+              <div className="slideContentContainer" ref={this.slideContainer}>
+                <div className="eventSlide red"> </div>
+                <div className="eventSlide blue"> </div>
+                <div className="eventSlide yellow"> </div>
+                <div className="eventSlide gr"> </div>
+                <div className="eventSlide wh"> </div>
+                <div className="eventSlide bk"> </div>
+              </div>
+            </div>
           </div>
-          <div className="freshPlan">
-            <div>신선플랜</div>
-          </div>
+          {/* <div onClick={() => this.slideNext()}>슬라이드 버튼</div> */}
+          {/* <div onClick={() => this.slidePrevious()}>슬라이드 버튼</div> */}
+          <div onClick={e => this.slideByNumber(e)}>0</div>
+          <div onClick={e => this.slideByNumber(e)}>1</div>
+          <div onClick={e => this.slideByNumber(e)}>2</div>
+          <div onClick={e => this.slideByNumber(e)}>3</div>
+          <div onClick={e => this.slideByNumber(e)}>4</div>
+          <div onClick={e => this.slideByNumber(e)}>5</div>
+          <div className="freshPlan">신선배송~</div>
         </section>
         <section className="bestItemContainer">
           <h5>정육각 베스트 상품</h5>
@@ -33,3 +93,5 @@ export default class Main extends Component {
     );
   }
 }
+
+const carousel = document.querySelector('.slideContentContainer');
