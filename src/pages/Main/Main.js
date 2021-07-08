b1d5e48c54cbb8f400cd79479ae4c8a3f768c30b
lee5992 <dlwogus432@gmail.com>
2021-07-08 17:43:40 +0900

    Fix : 슬라이드 함수 리팩토링

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -8,86 +8,52 @@ export default class Main extends Component {
     this.slideContainer = React.createRef();
   }
 
-  slideByNumber(e) {
-    this.slideContainer.current.style.transition =
-      'width 2s, height 2s, background-color 2s, transform 1s';
+  state = {
+    slideIndex: 0,
+  };
 
-    this.slideContainer.current.style.transform = `translateX(-${
-      576 * e.target.innerHTML
-    }px)`;
-
-    // console.log(this.slideContainer.current.style);
+  slideNext() {
+    this.setState({ slideIndex: this.state.slideIndex + 1 }, () => {
+      this.slideContainer.current.style.transition = '  transform 2s';
+      this.slideContainer.current.style.transform = `translateX(-${
+        1184 * this.state.slideIndex
+      }px)`;
+    });
   }
-  // slideNext() {
-  // console.log(carousel);
-  // let index = 0;
-  // if (index === 0) {
-  //   this.slideContainer.current.style.transform = `translateX(-${576}px)`;
-  //   index = index + 1;
-  // } else {
-  //   this.slideContainer.current.style.transform = `translateX(-${576}px)`;
-  //   index = index + 1;
-  // }
-  // console.log(this.slideContainer.current.style.transform);
-  // console.log(index);
-  // }
 
-  // slidePrevious() {
-  //   // console.log(carousel);
-
-  //   let index = 0;
-
-  //   if (index === 0) {
-  //     this.slideContainer.current.style.transform = `translateX(-${576 * 2}px)`;
-  //     index = index + 1;
-  //   } else {
-  //     this.slideContainer.current.style.transform = `translateX(+${576}px)`;
-  //     index = index + 1;
-  //   }
-  //   console.log(this.slideContainer.current.style.transform);
-  //   console.log(index);
-  // }
+  slidePrevious() {
+    this.setState({ slideIndex: this.state.slideIndex - 1 }, () => {
+      this.slideContainer.current.style.transition = ' transform 1s';
+      this.slideContainer.current.style.transform = `translateX(-${
+        1184 * this.state.slideIndex
+      }px)`;
+    });
+  }
 
   render() {
     return (
       <div className="mainPage">
-        <section className="mainBannerContainer">
-          <div className="mainBanner"></div>
-        </section>
-        <section className="eventBannerContainer">
-          <div className="mainEvent">
-            <div className="slideOverFlow">
-              <div className="slideContentContainer" ref={this.slideContainer}>
-                <div className="eventSlide red"> </div>
-                <div className="eventSlide blue"> </div>
-                <div className="eventSlide yellow"> </div>
-                <div className="eventSlide gr"> </div>
-                <div className="eventSlide wh"> </div>
-                <div className="eventSlide bk"> </div>
-              </div>
-            </div>
-            <div>
-              <div className="slideBtn" onClick={e => this.slideByNumber(e)}>
-                1
-              </div>
-              <div className="slideBtn" onClick={e => this.slideByNumber(e)}>
-                2
-              </div>
-              <div className="slideBtn" onClick={e => this.slideByNumber(e)}>
-                3
-              </div>
-              <div className="slideBtn" onClick={e => this.slideByNumber(e)}>
-                4
-              </div>
-              <div className="slideBtn" onClick={e => this.slideByNumber(e)}>
-                5
-              </div>
-            </div>
+        <div className="slideOverFlow">
+          <div className="slideContentContainer" ref={this.slideContainer}>
+            <div className="eventSlide red"> </div>
+            <div className="eventSlide blue"> </div>
+            <div className="eventSlide yellow"> </div>
+            <div className="eventSlide gr"> </div>
+            <div className="eventSlide wh"> </div>
+            <div className="eventSlide bk"> </div>
           </div>
-          {/* <div onClick={() => this.slideNext()}>슬라이드 버튼</div> */}
-          {/* <div onClick={() => this.slidePrevious()}>슬라이드 버튼</div> */}
-
-          <div className="freshPlan">신선배송~</div>
+        </div>
+        <div className="slideIndexBar">
+          <div className="slideBtn" onClick={() => this.slidePrevious()}>
+            {`<`}
+          </div>
+          <div>{this.state.slideIndex}</div>
+          <div className="slideBtn" onClick={() => this.slideNext()}>
+            {`>`}
+          </div>
+        </div>
+        <section className="eventBannerContainer">
+          <div className="mainEvent"></div>
         </section>
         <section className="bestItemContainer">
           <h5>정육각 베스트 상품</h5>
@@ -105,5 +71,3 @@ export default class Main extends Component {
     );
   }
 }
-
-const carousel = document.querySelector('.slideContentContainer');
