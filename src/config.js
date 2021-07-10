8a7f24ea2c69dfb349f76c00f83bae58c726d3db
lee5992 <dlwogus432@gmail.com>
2021-07-10 18:41:24 +0900

    Fix : 장바구니 HTTP 통신 기능 추가

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 const BASE_URL = 'http://10.58.7.59:8000';
+const LOGIN_BASE_URL = 'http://10.58.7.59:8000';
 
 export const API = {
   LIST: `${BASE_URL}/products/list`,
 };
+
+export const SIGNIN_API = `${LOGIN_BASE_URL}/signin`;
---
+++ src/pages/Cart/CartList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class CartList extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <ul>
+          <li className="itemList">
+            <img src={this.props.cartData.thumbnail} alt="itemIm" />
+            <div className="infoContainer">
+              <p className="productName">초신선 무항생제 돼지 삼겹살 구이용</p>
+              <p className="selectOption">보통(16mm)</p>
+            </div>
+            <p className="cartWeight">600g 기준</p>
+            <div className="counterContainer">
+              <div>
+                <i class="fas fa-minus"></i>
+              </div>
+              <p>1</p>
+              <div>
+                <i class="fas fa-plus"></i>
+              </div>
+            </div>
+            <div className="itemPrice">
+              <p>19,800원</p>
+            </div>
+            <button className="deleteBtn">x</button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
