70be9c35ba03f4e2a3b1d272ca59a0d0b4f70c33
lee5992 <dlwogus432@gmail.com>
2021-07-14 17:09:23 +0900

    Fix : 쿠폰사용관련문제 해결

--- src/components/Modal/CouponList.js
+++ src/components/Modal/CouponList.js
@@ -4,7 +4,7 @@ export default class CouponList extends Component {
   seletcAndClose = () => {
     this.props.selectCoupon(this.props.couponValue, this.props.couponId);
     this.props.toggleCoupon();
-    console.log(this.props.couponId);
+    console.log(this.props.couponId, '쿠폰을 선택했습니다.');
   };
   render() {
     return (
