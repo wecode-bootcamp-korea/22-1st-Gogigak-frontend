02cb584f625bc66ade2961dc4668c8ebf6060035
lee5992 <dlwogus432@gmail.com>
2021-07-14 17:21:51 +0900

    Fix : Cart.js 코드 리팩토링

--- src/components/Modal/CouponModal.js
+++ src/components/Modal/CouponModal.js
@@ -5,10 +5,13 @@ export default class CouponModal extends Component {
   render() {
     return (
       <section className="backgroundContainer">
-        <div className="modalContainer" onClick={e => e.stopPropagation()}>
+        <div
+          className="couponModalContainer"
+          onClick={e => e.stopPropagation()}
+        >
           <h3 className="modalTitle">쿠폰</h3>
 
-          <div className="optionContainer">
+          <div className="couponOptionContainer">
             <p className="couponOption">쿠폰선택</p>
             {this.props.couponData.map(x => (
               <CouponList
